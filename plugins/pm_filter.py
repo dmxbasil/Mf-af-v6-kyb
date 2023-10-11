66ff1826a71acc7df753c80a88272fbf85ffa437
BASIL » <77526924+dmxbasil@users.noreply.github.com>
2023-10-11 21:50:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -849,6 +849,8 @@ async def auto_filter(client, msg, spoll=False):
         if 2 < len(message.text) < 100:
             search = message.text
             files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
+		if not files:
+                await client.send_message(req_channel,f"•")]]))
                 l = await message.reply_text(text=f"Check Your spelling",)                                                                                      
                 await asyncio.sleep(60)
                 await l.delete()    
